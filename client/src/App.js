6a83cc3e69fb3244000a2fedc630df071ed7f04a
inkyant <80181074+inkyant@users.noreply.github.com>
2024-01-20 15:05:03 -0800

    handleClick

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 
-function ent() {
-  var x = document.getElementById("fname").value;
-  document.getElementById("demo").innerHTML = x;
-}
 
 function App() {
 
-  const [urlString, setUrlString] = useState("a");
+  const [urlString, setUrlString] = useState("");
+  const [infoString, setInfoString] = useState("");
+
+  function handleClick(url) {
+    setInfoString("your url: " + url)
+  }
 
   return (
     <div className="App">
@@ -26,10 +27,10 @@ function App() {
           <div className="content" role="main">
           <div className="inbx">
             <input type="text" onChange={(e) => setUrlString(e.target.value)} id="fname" name="fname" value={urlString} placeholder="TikTok Link.."/><br />
-            <button onclick={ent} >Enter</button>
+            <button onClick={() => handleClick(urlString)} >Enter</button>
           </div>
           <div className="outbx">
-            <p className="outtxt" id="demo">hello this is the output area</p>
+            <p className="outtxt" id="demo">{infoString}</p>
           </div>
           
           </div>
