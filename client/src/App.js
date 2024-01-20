e4306709b0eff712667cb7b6f22baff5b03e7044
Brennan0 <51968218+Brennan0@users.noreply.github.com>
2024-01-20 14:52:36 -0800

    helper function for input

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ent() {
+  var x = document.getElementById("fname").value;
+  document.getElementById("demo").innerHTML = x;
+}
+
 function App() {
   return (
-    
     <div className="App">
       <body>
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
-    <div class="titlebx">
-      <h1 class="title">Mrs. Info</h1>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
 
-    <div class="wrapper">
-      <div class="content" role="main">
-          <form class="inbx">
+        <div class="titlebx">
+          <h1 class="title">Mrs. Info</h1>
+        </div>
+
+        <div class="wrapper">
+          <div class="content" role="main">
+          <div class="inbx">
             <input type="text" id="fname" name="fname" value="" placeholder="TikTok Link.."/><br />
-            <input type="submit" value="Submit" />
-          </form>
-        
-      </div>
+            <button onclick={ent} >Enter</button>
+          </div>
+          <div class="outbx">
+            <p class="outtxt" id="demo">hello this is the output area</p>
+          </div>
+          
+          </div>
+        </div>
+
+        <footer class="footer">
       
-      <div class="out">
-        <form class="outbx">
-            <output name="x" for=""></output>
-        </form>
-      </div>
-    </div>
-    <footer class="footer">
-      <div class="links"></div>
-      
-    </footer>
-  </body>
+        </footer>
+      </body>
     </div>
   );
 }
