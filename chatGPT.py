fd85d5a9391b7bdf302b6fa854a76ae26da42e54
inkyant <80181074+inkyant@users.noreply.github.com>
2024-01-20 12:07:57 -0800

    chatGPT summary and article choosing

--- chatGPT.py
+++ chatGPT.py
@@ -1,12 +1,43 @@
+import re
 from api_keys import OPEN_AI_API_KEY
 from openai import OpenAI
 
 client = OpenAI(api_key=OPEN_AI_API_KEY)
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Say this is a test"}],
-    stream=False,
-)
+def get_summary(transcript):
 
-print(response.choices[0].message)
+    prompt = """Imagine you are an independent body that has to release a short summary of misinformation present within a video. The video transcript provided  below will be the video for which you will highlight any misinformation, in just 3 sentences.
+Additionally, after those three sentences, create a line break and type \"Search Terms:\" and then exactly 7 search terms that could be used to get specific information about the topic. Do not type anything else on that line. Only type one search query. Only type 7 words, do not include commas or multiple searches. For example:
+\"Search Terms: flat earth theory 2020 proven with gravity\""""
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt + "\n" + transcript}],
+        stream=False,
+    )
+
+    print(response.choices[0].message)
+    for res in response.choices:
+        if (re.match(r'unable to provide information', res.message.content or "") is None):
+            return res.message.content
+
+
+def get_article(transcript: str, articles: str):
+    prompt1 = """Consider this video transcript that may contain misinformation:"""
+    prompt2 = """Choose the most relevant of these snopes articles to the topic in the transcript. Respond with just one number, or -1 if none apply. For example, your response could be:1"""
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt1 + "\n" + transcript + "\n" + prompt2 + "\n" + articles}],
+        stream=False,
+    )
+
+    print(response.choices[0].message)
+
+    try:
+        if response.choices[0].message.content is not None:
+            return int(response.choices[0].message.content)
+        else:
+            print("message content is none")
+    except ValueError:
+        print("chatGPT did not return a number")
